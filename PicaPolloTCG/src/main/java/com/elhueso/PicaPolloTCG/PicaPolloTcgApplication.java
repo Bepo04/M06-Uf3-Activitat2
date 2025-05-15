a67cfdd89e9f435704578b1bf9e787a27e9a0239
Pau Lorca <2023_pau.lorca@iticbcn.cat>
2025-05-15 19:35:52 +0200

    initial fking commit

---
+++ PicaPolloTCG/src/main/java/com/elhueso/PicaPolloTCG/PicaPolloTcgApplication.java
@@ -0,0 +1,13 @@
+package com.elhueso.PicaPolloTCG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicaPolloTcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicaPolloTcgApplication.class, args);
+	}
+
+}
---
+++ PicaPolloTCG/src/test/java/com/elhueso/PicaPolloTCG/PicaPolloTcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elhueso.PicaPolloTCG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicaPolloTcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
