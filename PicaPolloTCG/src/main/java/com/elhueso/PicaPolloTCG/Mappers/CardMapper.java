d5b8ecab25dfbd57246c6c0c6addb51bfeececb5
andresgonzalez03 <2023_andres.gonzalez@iticbcn.cat>
2025-05-15 20:20:37 +0200

    fet mapper

---
+++ PicaPolloTCG/src/main/java/com/elhueso/PicaPolloTCG/Mappers/CardMapper.java
@@ -0,0 +1,15 @@
+package com.elhueso.PicaPolloTCG.Mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import com.elhueso.PicaPolloTCG.DTO.CardDTO;
+import com.elhueso.PicaPolloTCG.Model.Card;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CardMapper {
+
+    CardDTO toDTO(Card card);
+    Card toEntity(CardDTO cardDTO);
+
+}
