7417e242dc91c23768b1936237b7269013d5bda3
Marc <wavpunto@gmail.com>
2025-05-15 22:38:28 +0200

    RESTController done

--- PicaPolloTCG/src/main/java/com/elhueso/PicaPolloTCG/Controllers/RESTController.java
+++ PicaPolloTCG/src/main/java/com/elhueso/PicaPolloTCG/Controllers/RESTController.java
@@ -1,5 +1,62 @@
 package com.elhueso.PicaPolloTCG.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elhueso.PicaPolloTCG.DTO.CardDTO;
+import com.elhueso.PicaPolloTCG.Services.CardService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/")
 public class RESTController {
-    
+
+    private CardService cardService;
+
+    @Autowired
+    public RESTController(CardService cardService){
+        this.cardService = cardService;
+    }
+
+    @GetMapping(value = "/products")
+    public ResponseEntity<Flux<CardDTO>> getAllCards(){
+        return ResponseEntity.ok(cardService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Mono<CardDTO>> findById(@PathVariable String id){
+        Mono<CardDTO> cardDTO = cardService.findById(id);
+        if(cardDTO == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(cardDTO);
+    }
+
+    @PostMapping(value = "/products")
+    public ResponseEntity<Mono<CardDTO>> saveCard(@RequestBody CardDTO cardDTO){
+        Mono<CardDTO> saved = cardService.save(cardDTO);
+        return ResponseEntity.ok(saved); 
+    }
+
+    @PatchMapping(value = "/products")
+    public ResponseEntity<Mono<CardDTO>> updateCard(@RequestBody CardDTO cardDTO){
+        Mono<CardDTO> updated = cardService.update(cardDTO);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping(value = "/products")
+    public ResponseEntity<Mono<String>> deleteCard(@PathVariable String id){
+        cardService.delete(id);
+        return ResponseEntity.ok(Mono.just("La carta ha sigut borrada."));
+    }
 }
